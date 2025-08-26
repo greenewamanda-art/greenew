78d01ff440eba93931876ace6cd8d6aaedb13bb4
Willian Pereira <willianpereira0912@gmail.com>
2025-08-25 23:32:38 -0300

    teste api production

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,6 +1,6 @@
-import nodemailer from 'nodemailer';
-import type SMTPTransport from 'nodemailer/lib/smtp-transport';
-import { NextRequest, NextResponse } from 'next/server';
+// import nodemailer from 'nodemailer';
+// import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+// import { NextRequest, NextResponse } from 'next/server';
 
 // export async function POST(req: NextRequest) {
 //   try {
@@ -32,29 +32,13 @@ import { NextRequest, NextResponse } from 'next/server';
 // }
 
 
-export async function POST(req: Request) {
-  try {
-    const { name, from, message } = await req.json();
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
+import { NextRequest, NextResponse } from 'next/server';
 
-    await transporter.sendMail({
-      from: `GREENEW <${from}>`,
-      replyTo: from,
-      to: process.env.SMTP_TO,
-      subject: 'Contato Site',
-      html: `Nome: ${name} <br> E-mail: ${from} <br> Mensagem: ${message}`,
-    });
+export async function POST(req: NextRequest) {
+  return NextResponse.json({ message: 'API funcionando!' }, { status: 200 });
+}
 
-    return new Response(JSON.stringify({ message: 'Email enviado com sucesso!', status: true }), { status: 200 });
-  } catch (error: any) {
-    console.error('Erro:', error);
-    return new Response(JSON.stringify({ error: 'Erro ao processar requisição', details: error.message }), { status: 500 });
-  }
+export async function GET() {
+  return NextResponse.json({ message: 'GET funcionando!' }, { status: 200 });
 }
