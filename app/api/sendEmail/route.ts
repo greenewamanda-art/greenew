07de5233e9fdb3cd33267dab73cb583257708565
Willian Pereira <willianpereira0912@gmail.com>
2025-08-25 23:18:13 -0300

    NextRequest for debug in production

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -2,31 +2,59 @@ import nodemailer from 'nodemailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest) {
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { name, from, message } = await req.json();
+
+//     const transportOptions: SMTPTransport.Options = {
+//       service: 'gmail',
+//       auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS,
+//       },
+//     };
+
+//     const transporter = nodemailer.createTransport(transportOptions);
+
+//     await transporter.sendMail({
+//        from: `GREENEW ${from}`,
+//        replyTo: from,
+//        to: process.env.SMTP_TO,
+//        subject: 'Contato Site',
+//        html: `Nome: ${name} <br> E-mail: ${from} <br> Mensagem: ${message}`,
+//      });
+
+//     return NextResponse.json({ message: 'Email enviado com sucesso!', status: true,  }, { status: 200 });
+//   } catch (error: any) {
+//     console.error('Erro:', error);
+//     return NextResponse.json({ error: 'Erro ao processar requisição', details: error.message }, { status: 500 });
+//   }
+// }
+
+
+export async function POST(req: Request) {
   try {
     const { name, from, message } = await req.json();
 
-    const transportOptions: SMTPTransport.Options = {
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-    };
-
-    const transporter = nodemailer.createTransport(transportOptions);
+    });
 
     await transporter.sendMail({
-       from: `GREENEW ${from}`,
-       replyTo: from,
-       to: process.env.SMTP_TO,
-       subject: 'Contato Site',
-       html: `Nome: ${name} <br> E-mail: ${from} <br> Mensagem: ${message}`,
-     });
+      from: `GREENEW <${from}>`,
+      replyTo: from,
+      to: process.env.SMTP_TO,
+      subject: 'Contato Site',
+      html: `Nome: ${name} <br> E-mail: ${from} <br> Mensagem: ${message}`,
+    });
 
-    return NextResponse.json({ message: 'Email enviado com sucesso!', status: true,  }, { status: 200 });
+    return new Response(JSON.stringify({ message: 'Email enviado com sucesso!', status: true }), { status: 200 });
   } catch (error: any) {
     console.error('Erro:', error);
-    return NextResponse.json({ error: 'Erro ao processar requisição', details: error.message }, { status: 500 });
+    return new Response(JSON.stringify({ error: 'Erro ao processar requisição', details: error.message }), { status: 500 });
   }
 }
