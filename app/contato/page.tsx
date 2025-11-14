cd25c424598cfe924898de9433e8ba239325b278
greenewamanda-art <greenewamanda@gmail.com>
2025-11-14 15:05:57 -0300

    Update page.tsx

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -94,7 +94,7 @@ export default function ContatoPage() {
                 (55 11) 99564-4535 - Marina Porchat</p>
               <p className="text-lg flex items-center gap-2">
                 <svg width="28" height="28" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M341.8 72.6C329.5 61.2 310.5 61.2 298.3 72.6L74.3 280.6C64.7 289.6 61.5 303.5 66.3 315.7C71.1 327.9 82.8 336 96 336L112 336L112 512C112 547.3 140.7 576 176 576L464 576C499.3 576 528 547.3 528 512L528 336L544 336C557.2 336 569 327.9 573.8 315.7C578.6 303.5 575.4 289.5 565.8 280.6L341.8 72.6zM304 384L336 384C362.5 384 384 405.5 384 432L384 528L256 528L256 432C256 405.5 277.5 384 304 384z" /></svg>
-                Rua Jericó, 193 - CJ 32 - Vila Madalena<br className="hidden sm:block"></br>
+                Rua Jericó, 193 - CJ 32 - Vila Madalena<br className="hidden sm:block"></br>{" "}
                 São Paulo / SP - Brasil
               </p>
             </div>
